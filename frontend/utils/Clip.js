fa6f6fa2521c152376f693ca1cd3c397b3b4724e
Carl Kristof Tessier <carlkristoftessier@gmailcom>
2022-01-22 10:18:41 -0500

    Filled in gaps for AudioClip

--- frontend/utils/Clip.js
+++ frontend/utils/Clip.js
@@ -11,8 +11,12 @@ audio_stop.disabled = true;
 
 class Clip {
     
-    constructor(track) {
-        this.track=track;
+    constructor(track, start_pos, name) {
+        this.track = track;
+        this.start_pos = start_pos;
+        this.name = name;
+        this.to_string = "Clip " + name + 
+            "at " + this.track.id + ": " + this.start_pos;
     }
 
     record_clip() {
@@ -20,30 +24,47 @@ class Clip {
     }
 
     play() {
-        
+
     }
 }
 
 class AudioClip extends Clip{
     
-    constructor(track, record) {
+    constructor(track, start_pos, name) {
         this.track = track;
-        this.chunks = [];
-        // this.disabled = true;
-        // track.on_arm = function(e) {
-        //     this.disabled = false;
-        // }
-        
+        this.start_pos = start_pos;
+        this.name = name;
+        if (navigator.mediaDevices.getUserMedia({ audio: true })) {
+            console.log('getUserMedia supported.');
+
+            this.chunks = [];
+            this.mediaRecorder = new MediaRecorder(stream);
+        }
+        this.to_string = "Clip " + name + 
+            "at " + this.track.id + ": " + this.start_pos;
     }
 
-   
-
     // Override
-    record_clip() {
-        
+    start() {
+        console.log("Started recording clip in track " + this.track.id);
+
+        this.mediaRecorder.start();
+        this.mediaRecorder.addEventListener("dataavailable", event => {
+            this.chunks.push(event.data);
+        })
+    }
+
+    stop() {
+        console.log("Recording stopped");
+
+        mediaRecorder.stop();
     }
 
     play() {
-
+        console.log("Preparing and playing audio")
+        const blob = new Blob(this.chunks);
+        const audioUrl = URL.createObjectURL(blob);
+        const audio = new Audio(audioUrl);
+        audio.play();
     }
 }
